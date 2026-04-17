e6850f859414806f056aea52d3da7140dece35e9
Jul1xn <julian.verwoerd@pantronic.nl>
2026-04-17 11:58:27 +0200

    .env support for port

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const data = require("./data");
 const config = require("./constants");
+const dotenv = require("dotenv");
+dotenv.config();
 
 data.initializePageViews(config.tools);
 
@@ -27,6 +29,7 @@ app.use("/toolbox", toolboxRouter);
 const apiRouter = require("./routes/api");
 app.use("/api", apiRouter);
 
-app.listen(8081, () => {
-    console.log("Listening on http://localhost:8081");
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+    console.log(`Listening on http://localhost:${PORT}`);
 })
